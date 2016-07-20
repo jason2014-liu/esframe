62bf5b8e029a87ad5fce1734031ab56e356b1b61
jason2014-liu <379626076@qq.com>
2016-07-20 14:44:38 +0800

    esframe init

---
+++ src/main/resources/com/lmstudio/esframe/package-info.java
@@ -0,0 +1,16 @@
+/**
+* TODO
+* @Project: esframe
+* @Title: package-info.java
+* @Package com.lmstudio.esframe
+* @author jason.liu
+* @Date 2016年7月18日 下午4:57:18
+* @Copyright
+* @Version 
+*/
+/**
+* TODO
+* @ClassName: package-info
+* @author jason.liu
+*/
+package com.lmstudio.esframe;
